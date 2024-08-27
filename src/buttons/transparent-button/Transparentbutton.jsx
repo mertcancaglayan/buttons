1c6f34d2cc0eb85f4f482793511a8346b42bd4d6
M.can <caglayan.mertcan37@gmail.com>
2024-08-28 01:54:04 +0300

    add transparent buttons

---
+++ src/buttons/transparent-button/Transparentbutton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Transparentbutton.css";
+
+function Transparentbutton() {
+	return (
+		<div className="transparent-buttons buttonContainer">
+			<button id="button1">Button 1</button>
+			<button id="button2">Button 2</button>
+		</div>
+	);
+}
+
+export default Transparentbutton;
