c49f5f3b59b11449a6ffde56e3e21dca2f1f0a8f
M.can <caglayan.mertcan37@gmail.com>
2024-08-30 19:18:02 +0300

    add loading button

---
+++ src/buttons/loading-button/Loadingbutton.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Loadingbutton.css";
+
+function Loadingbutton() {
+	return (
+		<div className="buttonContainer">
+			<button className="loading-button">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+					{/* <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+					<path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z" />
+				</svg>
+				Loading...
+			</button>
+
+			<button className="loading-button">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="feather feather-loader"
+				>
+					<line x1="12" y1="2" x2="12" y2="6" />
+					<line x1="12" y1="18" x2="12" y2="22" />
+					<line x1="4.93" y1="4.93" x2="7.76" y2="7.76" />
+					<line x1="16.24" y1="16.24" x2="19.07" y2="19.07" />
+					<line x1="2" y1="12" x2="6" y2="12" />
+					<line x1="18" y1="12" x2="22" y2="12" />
+					<line x1="4.93" y1="19.07" x2="7.76" y2="16.24" />
+					<line x1="16.24" y1="7.76" x2="19.07" y2="4.93" />
+				</svg>
+				Loading...
+			</button>
+
+			<button className="loading-button">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					height="24px"
+					viewBox="0 -960 960 960"
+					width="24px"
+					fill="#black"
+				>
+					<path d="M522-80v-82q34-5 66.5-18t61.5-34l56 58q-42 32-88 51.5T522-80Zm-80 0Q304-98 213-199.5T122-438q0-75 28.5-140.5t77-114q48.5-48.5 114-77T482-798h6l-62-62 56-58 160 160-160 160-56-56 64-64h-8q-117 0-198.5 81.5T202-438q0 104 68 182.5T442-162v82Zm322-134-58-56q21-29 34-61.5t18-66.5h82q-5 50-24.5 96T764-214Zm76-264h-82q-5-34-18-66.5T706-606l58-56q32 39 51 86t25 98Z" />
+				</svg>
+				Loading...
+			</button>
+		</div>
+	);
+}
+
+export default Loadingbutton;
